7c22aad31fb867b39215b31904d06f21f6f0b855
Lean2811 <lean72428@gmail.com>
2024-11-27 09:11:30 +0700

    cau_11.py

---
+++ Bài4/cau_11.py
@@ -0,0 +1,8 @@
+print("le van an")
+print("235752021610044")
+ds = input("Nhap danh sach: ").split()
+if 'abc' in ds:
+    print("Vi tri cua chuoi 'abc' la:", ds.index('abc'))
+    ists
+else:
+    print("'abc' khong co trong danh sach")
