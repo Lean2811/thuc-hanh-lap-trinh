2ea30414ffb1707ccf14dab8246f7231bce6892c
Lean2811 <lean72428@gmail.com>
2024-11-27 09:18:04 +0700

    cau_13.py

---
+++ Bài4/cau_13.py
@@ -0,0 +1,6 @@
+print("le van an")
+print("235752021610044")
+chuoi_so_nhi_phan = input("Nhập chuỗi các số nhị phân: ")
+list_so_nhi_phan = chuoi_so_nhi_phan.split(',')
+for so in list_so_nhi_phan:
+    print(so)
