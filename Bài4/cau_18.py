f4a828d95e7d57b1e35bb4605ea3d9d59898145d
Lean2811 <lean72428@gmail.com>
2024-11-27 09:44:41 +0700

    cau_18.py

--- Bài4/cau_18.py
+++ Bài4/cau_18.py
@@ -4,17 +4,13 @@ def primes_less_than_one_million_sieve():
     limit = 1000000
     is_prime = [True] * limit
     primes = []
-
     for p in range(2, int(limit**0.5) + 1):
         if is_prime[p]:
             for i in range(p * p, limit, p):
                 is_prime[i] = False
-
     for p in range(2, limit):
         if is_prime[p]:
             primes.append(p)
-
     return tuple(primes)
-
 P = primes_less_than_one_million_sieve()
 print(P)
