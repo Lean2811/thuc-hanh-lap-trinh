8359ef1dfe285574e57c6f6ce695d4fbb9f5af0c
Lean2811 <lean72428@gmail.com>
2024-11-27 09:44:05 +0700

    cau_19.py

--- Bài4/cau_19.py
+++ Bài4/cau_19.py
@@ -7,10 +7,6 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
-
-# Tạo tuple chứa các số nguyên tố nhỏ hơn 1 triệu
 P = tuple(i for i in range(1_000_000) if is_prime(i))
-
-# Kiểm tra kết quả
-print(P[:10])  # In ra 10 số đầu tiên trong tuple
+print(P[:10])
 print(f"Số lượng số nguyên tố nhỏ hơn 1 triệu: {len(P)}")
