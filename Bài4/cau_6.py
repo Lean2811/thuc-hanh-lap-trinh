9c8a3d90ffd328ad37b038eb19eb8cad404d5c42
Lean2811 <lean72428@gmail.com>
2024-11-27 09:09:19 +0700

    cau_6.py

---
+++ Bài4/cau_6.py
@@ -0,0 +1,11 @@
+print("le van an")
+print("235752021610044")
+def get_sum(*num):
+    tmp = 0
+    # duyệt các tham số
+    for i in num:
+        tmp += i
+    return tmp
+
+result = get_sum(1, 2, 3, 4, 5)
+print(result)
