ea2429b7ebdcd5b56aed9fcab3410e59adf8118d
Lean2811 <lean72428@gmail.com>
2024-11-29 22:31:30 +0700

    cau 5_1.py

--- Bài 5/cau 5_1.py
+++ Bài 5/cau 5_1.py
@@ -1,1 +1,12 @@
-
+print("Le Van An")
+print("235752021610044")
+def square(n):
+    return n * n
+def cube(n):
+    return n * n * n
+def average(values):
+    nvals = len(values)
+    sum = 0.0
+    for v in values:
+        sum += v
+    return float(sum) / nvals
