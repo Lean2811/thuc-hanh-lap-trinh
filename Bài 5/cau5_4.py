e30116c13bd7ef93b923b213e68964637e8d75f5
Lean2811 <lean72428@gmail.com>
2024-12-02 09:56:28 +0700

    cau5_4.py

---
+++ Bài 5/cau5_4.py
@@ -0,0 +1,7 @@
+print("le van an")
+print("235752021610044")
+import numpy as np
+array = np.arange(12, 39)
+reversed_array = array[::-1]
+print("Mảng ban đầu:", array)
+print("Mảng đảo ngược:", reversed_array)
