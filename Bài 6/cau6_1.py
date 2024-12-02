afd53fc2d8813b2fab6983bc08d0a1e8891815c7
Lean2811 <lean72428@gmail.com>
2024-12-02 09:15:45 +0700

    cau6_1.py

--- Bài 6/cau6_1.py
+++ Bài 6/cau6_1.py
@@ -1,1 +1,11 @@
-
+print("le van an")
+print("235752021610044")
+class Circle(object):
+    def __init__(self, r):
+        self.radius = r  # Lưu trữ bán kính vào thuộc tính của đối tượng
+    def area(self):
+        return self.radius**2 * 3.14  # Tính diện tích hình tròn
+# Tạo một đối tượng hình tròn với bán kính 2
+circle = Circle(2)
+# Tính và in diện tích của hình tròn
+print(circle.area()
