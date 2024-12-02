88f34977cc5a52f90ffe534cbca1236e5c799f0a
Lean2811 <lean72428@gmail.com>
2024-12-02 09:36:42 +0700

    cau6_6.py

---
+++ Bài 6/cau6_6.py
@@ -0,0 +1,13 @@
+print("le van an")
+print("235752021610044")
+class StringHandler:
+    def __init__(self):
+        self.user_string = ""
+
+    def get_String(self):
+        self.user_string = input("Nhập chuỗi: ")
+    def print_String(self):
+        print(self.user_string.upper())
+string_handler = StringHandler()
+string_handler.get_String()
+string_handler.print_String()
