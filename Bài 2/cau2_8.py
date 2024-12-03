4bf7743bca20402fb39f32024abb37c106a7c238
Lean2811 <lean72428@gmail.com>
2024-12-03 20:37:03 +0700

    cau2_8.py

---
+++ Bài 2/cau2_8.py
@@ -0,0 +1,11 @@
+print("le van an")
+print("235752021610033")
+a, b = 1, 2
+total = 0
+print(a,end=" ")
+while (a <=4000000-1):
+    if a % 2 == 0:
+         total += a
+    a, bb, a+b
+    print(a,end="")
+print("\n sum of prime numbers term in fibonacci series: ",total)
