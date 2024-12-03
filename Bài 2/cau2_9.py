cfa949901601d1743a3b2486c662ac89dad7b6ed
Lean2811 <lean72428@gmail.com>
2024-12-03 20:54:53 +0700

    cau2_9.py

---
+++ Bài 2/cau2_9.py
@@ -0,0 +1,11 @@
+print("le van an")
+print("235752021610033")
+str=input("Enter a String:")
+dict = {}
+for n in str:
+    keys = dict.keys()
+    if n in keys:
+        dict [n] += 1
+    else:
+        dict [n] = 1
+print (dict)
