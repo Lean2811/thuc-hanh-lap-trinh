617b5c376e4f05188529fd16c725b2aa2d94f805
Lean2811 <lean72428@gmail.com>
2024-12-03 22:58:01 +0700

    cau8_4a.py

---
+++ Bài 8/cau8_4a.py
@@ -0,0 +1,15 @@
+"le van an")
+print("235752021610044")
+import tkinter as tk
+window = tk.Tk()
+window.title("Cửa sổ Tkinter Đơn Giản")
+window.geometry("300x200")
+label = tk.Label(window, text="Xin chào Tkinter!")
+label.pack(pady=20)
+
+button = tk.Button(window, text="Mở", command=lambda: label.config(text="Đã mở!"))
+button.pack(pady=10)
+
+button = tk.Button(window, text="Đóng", command=lambda: label.config(text="Đã đóng!"))
+button.pack(pady=10)
+window.mainloop()
