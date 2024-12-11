c5ce53e999f08763813b3cb07812a303aed49cb9
Lean2811 <lean72428@gmail.com>
2024-12-11 07:41:52 +0700

    cau5_8.mdule.py

---
+++ Bài 5/cau5_8.mdule.py
@@ -0,0 +1,20 @@
+print("le van an")
+print("235752021610044")
+n = int(input("Nhập số lượng phần tử trong danh sách: "))
+
+dlist = []
+for i in range(n):
+    element = int(input(f"Nhập phần tử thứ {i+1}: "))
+    dlist.append(element)
+
+item = int(input("Nhập phần tử cần tìm kiếm: "))
+
+import sequential_search
+
+found, index = sequential_search.Sequential_Search(dlist, item)
+
+# In kết quả
+if found:
+    print(f"Phần tử {item} được tìm thấy ở chỉ số {index}.")
+else:
+    print(f"Phần tử {item} không có trong danh sách.")
